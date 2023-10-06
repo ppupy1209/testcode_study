d6d9663ceaa8f01faa98185dbe24a6166fa634cb
yeonwoo <ppupy1209@naver.com>
2023-10-06 16:47:25 +0900

    음료 주문 어플리케이션 Spring 이용

---
+++ src/main/java/sample/cafekiosk/spring/service/product/ProductService.java
@@ -0,0 +1,29 @@
+package sample.cafekiosk.spring.service.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.service.product.response.ProductResponse;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<ProductResponse> getSellingProducts() {
+        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+
+        return products.stream()
+                .map(ProductResponse::of)
+                .collect(Collectors.toList());
+    }
+}
